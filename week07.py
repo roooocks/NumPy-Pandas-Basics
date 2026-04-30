06624317dba54e4a23375bb11a9ac59260926f1e
roooocks <dspdaw@gmail.com>
2026-04-30 14:50:48 +0900

    week09: pandas method chaining v0.2

--- week07.py
+++ week07.py
@@ -3,7 +3,7 @@ import random
 
 """
 만약 타입이 제각기로 들어간 파이썬 리스트를 array로 넣어 가장 상위 타입인 str로 변환한다.
- - 힘이 쌘 타입 : str > float > int 순이다.
+ - 힘이 쌘 타입 : str > float > int > bool 순이다.
 다만 리스트 안에 리스트 구조라면 차원은 1차원으로 바꿔야한다. (inhomogeneous, 1 dimensions)
 """
 
@@ -27,8 +27,9 @@ print(array03 > 0.5)
 
 # 4) 넘파이 배열 주요 통계함수
 array04 = np.arange(1, 11) # 1 ~ 10
-print(np.mean(array04))
-print(np.median(array04))
+print(array04)
+print(np.mean(array04)) # 평균
+print(np.median(array04)) # 중간값(짝수면 소수로 중간 값을 구한다. 100 200 300 400이면 250을 1부터10이라면 5와6사이의 5.5를)
 print(np.max(array04))
 print(np.min(array04))
 print(np.var(array04)) # 분산
--- week09.py
+++ week09.py
@@ -14,12 +14,16 @@ arr2d = np.array([
 
 
 # 02. pandas 2차원 배열 생성
-# 1차원: Serise / 2차원: DataFrame
-# df_2dlist = pd.DataFrame(arr2d, columns=['국', '영', '수'], index=[1, 2, 3])
+# 1차원: Serise / 2차원: DataFrame(list or dict로 생성)
+# df_2dlist = pd.DataFrame(arr2d, columns=['국', '영', '수'], index=[1, 2, 3]) # 이거 1차원이다.
 df_2dlist = pd.DataFrame([[1, 2, 3], [6, 4, 5], [7, 8, 9]], columns=['국', '영', '수'], index=[1, 2, 3])
 df_dict = pd.DataFrame({'국': [1, 6, 7], '영': [2, 4, 8], '수': [3, 5, 9]}, index=[1, 2, 3])
-print(df_2dlist)
+# print(df_2dlist)
 print(df_dict)
 
 
-# 03. pandas 
+# 03. pandas 메소드 체이닝
+# ClassName().setName("kim").setAge(30).setJob("doctor")... 방식을 쓰는 경우가 메소드 체이닝이다.
+# df_new = pd.melt(df_dict) # 첫번째 방식
+df_new = pd.melt(df_dict).rename(columns={'variable': 'var', 'value': 'val'}).query('val > 5')
+print(df_new)
